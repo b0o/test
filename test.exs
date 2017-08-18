945ae2e71dada41b4bf6c0376176ac8f7f81c2c2
Maddison Hellstrom <maddy@thundr.net>
2017-08-17 21:31:16 -0700

    Simplify syntax

--- test.exs
+++ test.exs
@@ -1,10 +1,7 @@
 defmodule Test do
   def f(n, l) do
-    Stream.concat(Stream.map(0..n, fn(i) ->
-      List.duplicate(i, l)
-    end))
+    Stream.concat Stream.map 0..n, &List.duplicate(&1, l)
   end
 end
 
-IO.puts(Enum.count(Test.f(10000, 10000)))
-
+IO.puts Enum.count Test.f(10000, 10000)
